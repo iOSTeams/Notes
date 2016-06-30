aa32397ea5f67a4bc157de57a170748bb156d0b9
Justin Lee <jlee402@mail.ccsf.edu>
2016-06-30 09:58:14 -0700

    Initial Commit, test cells

---
+++ Notes/Controllers/DisplayNotesTableViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DisplayNotesTableViewController.swift
+//  Notes
+//
+//  Created by King Justin on 6/29/16.
+//  Copyright © 2016 justinleesf. All rights reserved.
+//
+
+import UIKit
+
+class DisplayNotesTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
---
+++ Notes/Views/ListNotesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListNotesTableViewCell.swift
+//  Notes
+//
+//  Created by King Justin on 6/29/16.
+//  Copyright © 2016 justinleesf. All rights reserved.
+//
+
+import UIKit
+
+class ListNotesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
